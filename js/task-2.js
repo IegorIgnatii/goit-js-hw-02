d2b5825893a3eb09f9d0e859a207596f2740553f
IegorIgnatii <egor.ignatiy@gmail.com>
2024-06-16 22:06:16 +0300

    2t

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,10 @@
 function formatMessage(message, maxLength) {
     
+    if (message.length <= maxLength) {
+        console.log(message)
+    } else (message.length > maxLength) {
+        console.log(message + '...')
+    }
 }
 
 
