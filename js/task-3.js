43ab4c2c4ca698c6c79dc572dbea32f87b2f0791
IegorIgnatii <egor.ignatiy@gmail.com>
2024-06-19 23:03:09 +0300

    go2

--- js/task-3.js
+++ js/task-3.js
@@ -1,31 +1,28 @@
 debugger;
 
+function checkForSpam(message) {
+    if (message.toUpperCase().includes('SPAM' && 'SALE')){
+         return true} else {
+    return false
+}
+}
+
+
 // function checkForSpam(message) {
-//     if (message.toUpperCase().includes('SPAM' && 'SALE')){
-//          return true} else {
-//     return false
+//     if (message.toUpperCase().includes('SPAM'))
+//     { return true }
+//     else if (message.toUpperCase().includes('SALE'))
+//     { return true }
+//     else
+//     { return false }
 // }
-// }
-
-function checkForSpam(message) {
-    if (message.toUpperCase().includes('SPAM'))
-    { return true }
-    else if (message.toUpperCase().includes('SALE'))
-    { return true }
-    else
-    { return false }
-}
-
-function checkForSpam(message) {
-   
-}
 
 
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
